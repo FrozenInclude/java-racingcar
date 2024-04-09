c63997de9bbda7bd8bf79d7b77f69cfa2e17e868
FrozenInclude <abc6271416@naver.com>
2024-04-09 09:49:00 +0900

    car 클래스의 toString 오버라이드 정의

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -6,16 +6,13 @@ public class Car {
     private int movement=0;
     public String name;
 
+    @Override
+    public String toString() {
+        return name+" : "+"-".repeat(movement)+"\n";
+    }
     public void move(){
         int rnum=pickNumberInRange(0,9);
         if(rnum>=4)movement++;
-    }
-    public void print_move(){
-        int loop=movement;
-        System.out.print(name+" : ");
-        while (loop-->0)
-            System.out.print('-');
-        System.out.println();
     }
     public int getMovement(){
         return movement;
